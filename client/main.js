59ecbec48122385f8701022c1f4c3387ea1e9abb
Ben Slabbert <ben@hex.co.za>
2017-12-23 12:52:10 +0200

    v4 react router

--- client/main.js
+++ client/main.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './components/App';
 import Bins from '../import/collections/Bins';
 
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path={'/'} component={App}/>
+        </div>
+    </BrowserRouter>
+);
+
 Meteor.startup( () => {
-    ReactDOM.render( <App/>, document.querySelector( '.render-target' ) )
+    ReactDOM.render( routes, document.querySelector( '.render-target' ) )
 } );
