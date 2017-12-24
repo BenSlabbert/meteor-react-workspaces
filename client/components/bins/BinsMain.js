e1e1d1b4ecd8857ce3b082ff7dacff62967c9c08
Ben Slabbert <ben@hex.co.za>
2017-12-24 13:46:00 +0200

    getting bin from the url

--- client/components/bins/BinsMain.js
+++ client/components/bins/BinsMain.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Bins } from '../../../import/collections/Bins';
 
 class BinsMain extends Component {
 
-    constructor( props ) {
-        super( props );
-    }
-
     render() {
+        console.log( this.props.bin );
         return (
             <div>bins main</div>
         );
     }
 }
 
-export default BinsMain;
+export default createContainer( ( props ) => {
+    const { binId } = props.match.params;
+    Meteor.subscribe( 'bins' );
+
+    return {
+        bin: Bins.findOne( binId )
+    }
+}, BinsMain );
